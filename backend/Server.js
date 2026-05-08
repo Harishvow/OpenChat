1b300467a277b6c1fb70d06f1fa3a16af8592230
Harishvow <harisharish982005@gmail.com>
2026-05-08 20:05:48 +0530

    Fix: update chat route to serve index.html instead of chat.html

--- backend/Server.js
+++ backend/Server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 app.get('/chat/:chatId', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/chat.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 app.use('/', chatLink);
 
