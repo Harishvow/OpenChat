17670f60b6decfe10db6fb5c9dbfb197af0e61ad
Harishvow <harisharish982005@gmail.com>
2026-05-09 12:39:47 +0530

    Fix: update message fetching route to include API prefix

--- backend/routes/chatLink.js
+++ backend/routes/chatLink.js
@@ -7,6 +7,6 @@ const { getMessages } = require('../controllers/MessagePage');
 
 router.get('/create-chatLink', createChat);
 router.get('/chat/:chatId', checkChat, loadChatPage);
-router.get('/messages/:chatId', getMessages);
+router.get('/api/messages/:chatId', getMessages);
 
 module.exports = router;
